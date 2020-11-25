930fe75a14f1674cfe5f38635c058c094ce450ad
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-11-25 09:16:07 +0100

    import initial trait definition
    Only support unit for now
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+extern crate alloc;
+
+use alloc::boxed::Box;
+use core::{future::Future, ops::Deref, pin::Pin};
+
+pub trait Executor {
+    fn spawn(&self, f: Pin<Box<dyn Future<Output = ()> + Send>>);
+    fn spawn_blocking(&self, f: Box<dyn FnOnce() + Send>);
+}
+
+impl<E: Deref> Executor for E
+where
+    E::Target: Executor,
+{
+    fn spawn(&self, f: Pin<Box<dyn Future<Output = ()> + Send>>) {
+        self.deref().spawn(f);
+    }
+
+    fn spawn_blocking(&self, f: Box<dyn FnOnce() + Send>) {
+        self.deref().spawn_blocking(f)
     }
 }
