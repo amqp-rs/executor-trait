af6ed0054e9a9f20827a6db91a92a60c9b68ef54
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2021-01-05 16:08:47 +0100

    split BlockingExecutor, add FullExecutor
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,18 @@ pub struct LocalExecutorError(pub Pin<Box<dyn Future<Output = ()>>>);
 
 impl fmt::Debug for LocalExecutorError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("LocalExecutorError").field(&"Future").finish()
+        f.debug_tuple("LocalExecutorError")
+            .field(&"Future")
+            .finish()
     }
 }
 
 /// A common interface for spawning futures and blocking tasks on top of an executor
-#[async_trait]
+pub trait FullExecutor: Executor + BlockingExecutor {}
+
+impl<E: Executor + BlockingExecutor> FullExecutor for E {}
+
+/// A common interface for spawning futures on top of an executor
 pub trait Executor {
     /// Block on a future until completion
     fn block_on(&self, f: Pin<Box<dyn Future<Output = ()>>>);
@@ -29,15 +35,21 @@ pub trait Executor {
     fn spawn(&self, f: Pin<Box<dyn Future<Output = ()> + Send>>) -> Box<dyn Task>;
 
     /// Spawn a non-Send future on the current thread and return a handle to track its completion.
-    fn spawn_local(&self, f: Pin<Box<dyn Future<Output = ()>>>) -> Result<Box<dyn Task>, LocalExecutorError> {
+    fn spawn_local(
+        &self,
+        f: Pin<Box<dyn Future<Output = ()>>>,
+    ) -> Result<Box<dyn Task>, LocalExecutorError> {
         Err(LocalExecutorError(f))
     }
+}
 
+/// A common interface for spawning blocking tasks on top of an executor
+#[async_trait]
+pub trait BlockingExecutor {
     /// Convert a blocking task into a future, spawning it on a decicated thread pool
     async fn spawn_blocking(&self, f: Box<dyn FnOnce() + Send + 'static>);
 }
 
-#[async_trait]
 impl<E: Deref + Sync> Executor for E
 where
     E::Target: Executor + Sync,
@@ -50,10 +62,19 @@ where
         self.deref().spawn(f)
     }
 
-    fn spawn_local(&self, f: Pin<Box<dyn Future<Output = ()>>>) -> Result<Box<dyn Task>, LocalExecutorError> {
+    fn spawn_local(
+        &self,
+        f: Pin<Box<dyn Future<Output = ()>>>,
+    ) -> Result<Box<dyn Task>, LocalExecutorError> {
         self.deref().spawn_local(f)
     }
+}
 
+#[async_trait]
+impl<E: Deref + Sync> BlockingExecutor for E
+where
+    E::Target: BlockingExecutor + Sync,
+{
     async fn spawn_blocking(&self, f: Box<dyn FnOnce() + Send + 'static>) {
         self.deref().spawn_blocking(f).await
     }
